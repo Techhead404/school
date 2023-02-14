c925b5cd6b2fa89dca5195f3fd72ce5c5cf00629
Dillon Greek <techhead404@gmail.com>
2023-02-13 21:45:23 -0600

    js paging finished needs uploaded to onedrive

--- webdev/js_page/main.js
+++ webdev/js_page/main.js
@@ -1,3 +1,7 @@
 function test(){
-    console.log(document.getElementById("iFirstName").className);
+    document.getElementById("row").className = "row mx-auto mt-5 w-50 text-center bg-dark text-light";
+    let output = document.forms["form"]["iFirstName"].value;
+    document.getElementById("outputName").innerText = document.forms["form"]["iFirstName"].value + " " + document.forms["form"]["iLastName"].value;
+    document.getElementById("outputEmail").innerText = document.forms["form"]["iEmail"].value; 
+    document.getElementById("outputSub").innerText = document.forms["form"]["subCheckBox"].checked;
 }
