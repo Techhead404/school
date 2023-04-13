fc0a998fb71689fb2a0a9ab58c7e504cea29ddcf
Dillon Greek <techhead404@gmail.com>
2023-04-12 22:21:49 -0500

    JS finished still needs style

--- webdev/Rocket_Launch/app.js
+++ webdev/Rocket_Launch/app.js
@@ -1,52 +1,53 @@
 // https://ll.thespacedevs.com/2.2.0/swagger/#/launch/launch_list
 
+// Dillon Greek
+// Date: 04/12/23
+// Class: CIS-131
+// Rocket Launch
 
+// for making 2 request
 var httpRequest;
+var httpRequest2;
 
-document.getElementById("getRocket").addEventListener('click', () => {
+window.onload =() => {
+    //make new intance of XMLHttpRequest
     httpRequest = new XMLHttpRequest();
+    //get the json from api
     httpRequest.open("GET", "https://lldev.thespacedevs.com/2.2.0/launch/")
     httpRequest.send();
-    httpRequest.onreadystatechange = rocketFunction;
-    
-})
+    //check in state has changed
+    httpRequest.onreadystatechange = rocketFuntion;
 
-document.getElementById("getRocket").addEventListener('click', () => {
-    httpRequest = new XMLHttpRequest();
-    httpRequest.open("GET", "https://lldev.thespacedevs.com/2.2.0/launch/")
-    httpRequest.send();
-    httpRequest.onreadystatechange = astroFunction;
-    
-})
+    httpRequest2 = new XMLHttpRequest();
+    httpRequest2.open("GET", "https://lldev.thespacedevs.com/2.2.0/astronaut/")
+    httpRequest2.send();
+    httpRequest2.onreadystatechange = astroFuntion;  
+};
+
+//Get rocket info and img url
 function rocketFuntion(){
+    //Chech state code and status
     if (httpRequest.readyState == 4 && httpRequest.status == 200){
-        var astro = httpRequest.responseText;
-
-        const astroobj = JSON.parse(astro);
-        //console.log(rocketobj.results[0]);
-
+        var rocket = httpRequest.responseText;
+        //parse json into array
+        const rocketobj = JSON.parse(rocket);
+        //loop 4 times grabbing objects based on index i
         for(var i = 1; i < 5; i++){
-            console.log(astroObj);
-            // let rocketimg = document.getElementById("rocket"+[i])
-            // let rocketspn = document.getElementById("rocket"+[i]+"spn");
-            // rocketimg.src = rocketobj.results[i].image;
-            // rocketspn.innerHTML = rocketobj.results[i].name;
+            let rocketimg = document.getElementById("rocket"+[i])
+            rocketimg.src = rocketobj.results[i].image;
         }
     }
 }
-function astroFuntion(){
-    if (httpRequest.readyState == 4 && httpRequest.status == 200){
-        var rocket = httpRequest.responseText;
 
-        const rocketobj = JSON.parse(rocket);
-        //console.log(rocketobj.results[0]);
+//Get Astronout info and img
+function astroFuntion(){
+    if (httpRequest2.readyState == 4 && httpRequest2.status == 200){
+        var astro = httpRequest2.responseText;
+        const astroobj = JSON.parse(astro);
 
         for(var i = 1; i < 5; i++){
-            console.log("rocket"+[i]);
-            let rocketimg = document.getElementById("rocket"+[i])
-            let rocketspn = document.getElementById("rocket"+[i]+"spn");
-            rocketimg.src = rocketobj.results[i].image;
-            rocketspn.innerHTML = rocketobj.results[i].name;
+            let astroimg = document.getElementById("astro"+[i])
+            astroimg.src = astroobj.results[i].profile_image;
         }
     }
 }
